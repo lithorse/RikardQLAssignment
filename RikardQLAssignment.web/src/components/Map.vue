8d9acec3d2e2abe43803708eeef66abc972849ac
Rikard Hohlfält <rikard.hohlfalt@gmail.com>
2019-04-26 11:25:03 +0200

    Adds vue2-google-maps, Displays map and can add and clear markers

---
+++ RikardQLAssignment.web/src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="getMarkers">Read</button>
+    <button @click="clearMarkers">Clear</button>
+    <GmapMap :center="center" :zoom="3" map-type-id="terrain" style="width: 100%; height: 900px" :options="{fullscreenControl: false, streetViewControl: false}">
+        <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" @click="center=m.position"/>
+    </GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data(){
+      return {
+        center: { lat: 0, lng: 0 },
+        markers: [],
+        places: [],
+        currentPlace: null
+      }
+  },
+  methods: {
+      getMarkers(){
+          //TODO: Get markers from backend
+          this.markers.push({position: { lat: 45.508, lng: -73.587 }}, {position: { lat: 28.508, lng: -28.587 }})
+      },
+      clearMarkers(){
+          this.markers = []
+      }
+  }
+}
+</script>
--- RikardQLAssignment.web/src/main.js
+++ RikardQLAssignment.web/src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCfDeFvq16sqrmyRSWzcvBHoQtEpsGNVOE',
+    libraries: 'places'
+  }
+})
 
 Vue.config.productionTip = false
 
