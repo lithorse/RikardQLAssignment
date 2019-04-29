72681567ba8ed1e8106b37b6d01fe0676b80e5f0
Rikard Hohlfält <rikard.hohlfalt@gmail.com>
2019-04-29 15:27:06 +0200

    Can input coordinates to set custom marker

--- RikardQLAssignment.web/src/components/Map.vue
+++ RikardQLAssignment.web/src/components/Map.vue
@@ -4,6 +4,13 @@
         <button class="button is-medium" @click="getMarkers">Read</button>
         <button class="button is-medium" @click="clearMarkers">Clear</button>
         <button class="button is-medium" @click="getCurrentLocation">My Position</button>
+        <div class="field"> 
+          <label class="label">Latitude</label>
+          <input class="input" placeholder="Latitude" v-model="inputLat">
+          <label class="label">Longitude</label>
+          <input class="input" placeholder="Longitude" v-model="inputLng">
+        </div>
+        <button class="button is-medium" @click="setCustomMarker(inputLat, inputLng)">Set Marker</button>
       </div>
     <GmapMap ref="gmap" :center="center" :zoom="3" map-type-id="hybrid" style="width: 100%; height: 100vh" :options="{fullscreenControl: false, streetViewControl: false, mapTypeControl: false}">
         <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :title="m.title" :clickable="true" @click="center=m.position"/>
@@ -20,7 +27,9 @@ export default {
         center: { lat: 0, lng: 0 },
         markers: [],
         counter: 0,
-        urlAppend: ''
+        urlAppend: '',
+        inputLat: null,
+        inputLng: null
       }
   },
   methods: {
@@ -37,11 +46,12 @@ export default {
       setMarkers(data){
             this.markers = [];
             data.forEach(position => {
-                this.addMarker({position: { lat: parseFloat(position.latitude), lng: parseFloat(position.longitude) }, title: position.title})
+                this.addMarker(parseFloat(position.latitude), parseFloat(position.longitude), position.title)
             });
             this.setMapBounds();
       },
-      addMarker(marker){
+      addMarker(latitude, longitude, title){
+        var marker = {position: { lat: latitude, lng: longitude }, title: title}
         this.markers.push(marker)
       },
       clearMarkers(){
@@ -51,10 +61,7 @@ export default {
         var self = this;
         if (navigator.geolocation){
           navigator.geolocation.getCurrentPosition(function(position) {
-            self.addMarker({
-              position: { lat: position.coords.latitude, lng: position.coords.longitude }, 
-              title: 'lat: ' + position.coords.latitude + ' lng: ' + position.coords.longitude
-            })
+            self.addMarker(position.coords.latitude, position.coords.longitude, 'Latitude: ' + position.coords.latitude + ' Longitude: ' + position.coords.longitude)
             self.setMapBounds();
           }, 
           function() {
@@ -76,6 +83,10 @@ export default {
           }
           map.fitBounds(bounds);
         });
+      },
+      setCustomMarker(latitude, longitude){
+        this.addMarker(parseFloat(latitude), parseFloat(longitude), 'Latitude: ' + latitude + ' Longitude: ' + longitude)
+        this.setMapBounds()
       }
   }
 }
