d79d6428d26c8f10df1ea4c69233ba85e7aa09bb
Rikard Hohlfält <rikard.hohlfalt@gmail.com>
2019-04-29 14:45:41 +0200

    Adds button to set marker on browser geolocation

--- RikardQLAssignment.web/src/components/Map.vue
+++ RikardQLAssignment.web/src/components/Map.vue
@@ -3,6 +3,7 @@
       <div class="buttons">
         <button class="button is-medium" @click="getMarkers">Read</button>
         <button class="button is-medium" @click="clearMarkers">Clear</button>
+        <button class="button is-medium" @click="getCurrentLocation">My Position</button>
       </div>
     <GmapMap ref="gmap" :center="center" :zoom="3" map-type-id="hybrid" style="width: 100%; height: 100vh" :options="{fullscreenControl: false, streetViewControl: false, mapTypeControl: false}">
         <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :title="m.title" :clickable="true" @click="center=m.position"/>
@@ -36,18 +37,45 @@ export default {
       setMarkers(data){
             this.markers = [];
             data.forEach(position => {
-                this.markers.push({position: { lat: parseFloat(position.latitude), lng: parseFloat(position.longitude) }, title: position.title})
+                this.addMarker({position: { lat: parseFloat(position.latitude), lng: parseFloat(position.longitude) }, title: position.title})
             });
-            this.$refs.gmap.$mapPromise.then((map) => {
-                const bounds = new google.maps.LatLngBounds()
-                for (let m of this.markers) {
-                    bounds.extend(m.position)
-                }
-                map.fitBounds(bounds);
-            });
+            this.setMapBounds();
+      },
+      addMarker(marker){
+        this.markers.push(marker)
       },
       clearMarkers(){
           this.markers = []
+      },
+      getCurrentLocation(){
+        var self = this;
+        if (navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(function(position) {
+            self.addMarker({
+              position: { lat: position.coords.latitude, lng: position.coords.longitude }, 
+              title: 'lat: ' + position.coords.latitude + ' lng: ' + position.coords.longitude
+            })
+            self.setMapBounds();
+          }, 
+          function() {
+            handleLocationError(true);
+          });
+        } 
+        else {
+          handleLocationError(false);
+        }
+      },
+      handleLocationError(browserHasGeolocation) {
+        browserHasGeolocation ? console.log('Error: The Geolocation service failed.') : console.log('Error: Your browser doesn\'t support geolocation.');
+      },
+      setMapBounds(){
+        this.$refs.gmap.$mapPromise.then((map) => {
+          const bounds = new google.maps.LatLngBounds()
+          for (let m of this.markers) {
+            bounds.extend(m.position)
+          }
+          map.fitBounds(bounds);
+        });
       }
   }
 }
