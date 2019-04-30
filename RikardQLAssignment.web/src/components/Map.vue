5d56d217abaf72dc1fbef452eaa6c171d420924e
Rikard Hohlfält <rikard.hohlfalt@gmail.com>
2019-04-30 10:32:13 +0200

    Makes the buttons and inputs prettier

--- RikardQLAssignment.web/src/components/Map.vue
+++ RikardQLAssignment.web/src/components/Map.vue
@@ -4,13 +4,15 @@
         <button class="button is-medium" @click="getMarkers">Read</button>
         <button class="button is-medium" @click="clearMarkers">Clear</button>
         <button class="button is-medium" @click="getCurrentLocation">My Position</button>
-        <div class="field"> 
-          <label class="label">Latitude</label>
-          <input class="input" placeholder="Latitude" v-model="inputLat">
-          <label class="label">Longitude</label>
-          <input class="input" placeholder="Longitude" v-model="inputLng">
+        <label class="subtitle label">Latitude:</label>
+        <div class="field margin-left-8px"> 
+          <input class="input is-medium" placeholder="Latitude" v-model="inputLat">
         </div>
-        <button class="button is-medium" @click="setCustomMarker(inputLat, inputLng)">Set Marker</button>
+        <label class="subtitle label margin-left-8px">Longitude:</label>
+        <div class="field margin-left-8px"> 
+          <input class="input is-medium" placeholder="Longitude" v-model="inputLng">
+        </div>
+        <button class="button is-medium is-primary margin-left-8px" @click="setCustomMarker(inputLat, inputLng)">Set Marker</button>
       </div>
     <GmapMap ref="gmap" :center="center" :zoom="3" map-type-id="hybrid" style="width: 100%; height: 100vh" :options="{fullscreenControl: false, streetViewControl: false, mapTypeControl: false}">
         <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :title="m.title" :clickable="true" @click="center=m.position"/>
